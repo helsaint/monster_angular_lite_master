2a745fba4b5b6487bf8b37740669b39fd6328c80
helsaint <alexei.ramotar@gmail.com>
2023-04-18 13:37:23 -0400

    Empty choropleth map added

--- src/app/dashboard/dashboard-components/scatterplot/scatterplot.component.ts
+++ src/app/dashboard/dashboard-components/scatterplot/scatterplot.component.ts
@@ -9,11 +9,46 @@ import * as topojson from 'topojson';
   styleUrls: ['./scatterplot.component.scss']
 })
 export class ScatterplotComponent implements OnInit {
+  private svg: any
+  private margin = 50;
+  private width = 400;
+  private height = 300;
 
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    d3.json('assets/json/world.json').then((data:any) => {
+      const world = topojson.feature(data, data.objects.countries);
+      this.drawMap(world);
+    })
 
   }
 
+  private drawMap(topo:any): void{
+    this.svg = d3.select("figure#map")
+    .append("svg")
+    .attr("width", this.width)
+    .attr("height", this.height);
+
+    let path = d3.geoPath();
+    let projection = d3.geoMercator()
+    .scale(70)
+    .center([0,20])
+    .translate([this.width/2, this.height/2]);
+    let colorScale = d3.scaleThreshold()
+    .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000]);
+
+    this.svg.append("g")
+    .selectAll("path")
+    .data(topo.features)
+    .enter()
+    .append("path")
+      .attr("d", path
+      .projection(projection)
+      );
+
+
+    
+  }
+  
 }
